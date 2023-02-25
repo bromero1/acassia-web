919394a9cc219ada72dc7a3ad392415c112eb9ec
bryan r <bromero52@gmail.com>
2023-02-24 20:25:57 -0500

    list

--- client/src/scenes/home/ShoppingList.jsx
+++ client/src/scenes/home/ShoppingList.jsx
@@ -15,24 +15,51 @@ const ShoppingList = () => {
   const handleChange = (event, newValue) => {
     setValue(newValue);
   };
-
   async function getItems() {
     const items = await fetch(
       "http://localhost:1337/api/items?populate=image",
-      { method: "GET" }
+      {
+        method: "GET",
+      }
     );
     const itemsJson = await items.json();
     dispatch(setItems(itemsJson.data));
   }
 
   useEffect(() => {
-    getItems()
+    getItems();
   }, []);
 
+  // FILTERS FOR ITEMS
+  // ---------------------------------------------
+  const topRatedFilter = items.filter(
+    (item) => item.attributes.category === "topRated"
+  );
+
+  const newArrivalsFilter = items.filter(
+    (item) => item.attributes.category === "newArrivals"
+  );
+
+  const bestSellersFilter = items.filter(
+    (item) => item.attributes.category === "bestSellers"
+  );
+
+  // ---------------------------------------------
+
   return (
-    <div>
-      <h1>Shopping List</h1>
-    </div>
+    <Box width="80%" margin="80px auto">
+      <Typography variant="h2" textAlign="center">
+        Take your pick
+      </Typography>
+
+      <Tabs onChange={handleChange} value={value} centered>
+        <Tab label="NEW ARRIVALS" value="newArrivals"></Tab>
+        <Tab label="BEST SELLERS" value="bestSellers"></Tab>
+        <Tab label="TOP RATED" value="topRated"></Tab>
+        <Tab label="ALL" value="all"></Tab>
+
+      </Tabs>
+    </Box>
   );
 };
 
