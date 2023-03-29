1fc65bc4475d856c3f92d14dae6f3c6a05f0a340
bryan r <bromero52@gmail.com>
2023-03-29 10:22:38 -0400

    Item page

--- client/src/scenes/itemDetails/ItemDetails.jsx
+++ client/src/scenes/itemDetails/ItemDetails.jsx
@@ -1,9 +1,38 @@
-import React from 'react'
+import React, { useEffect } from "react";
+import { useState } from "react";
+import { useDispatch } from "react-redux";
+import { IconButton, Box, Typography, useTheme, Button } from "@mui/material";
+import AddIcon from "@mui/icons-material/Add";
+import RemoveIcon from "@mui/icons-material/Remove";
+import { shades } from "../../theme";
+import { addToCart } from "../../state";
+import { useNavigate, useParams } from "react-router-dom";
+import { useLocation } from "react-router-dom";
 
 const ItemDetails = () => {
-  return (
-    <div>ItemDetails</div>
-  )
-}
+  let { itemId } = useParams();
 
-export default ItemDetails
+  const getItem = async () => {
+    const response = await fetch(`http://localhost:1337/api/items/${itemId}`, {
+      method: "GET",
+    });
+
+    const jsonData = await response.json();
+  };
+
+  useEffect(() => {
+    getItem();
+  }, []);
+
+  return (
+    <Box width="80%" margin="80px auto">
+      <Box display="flex" flexWrap="wrap" columnGap="40px">
+        <Box backgroundColor="red" width="50"></Box>
+
+        <Box backgroundColor="red"></Box>
+      </Box>
+    </Box>
+  );
+};
+
+export default ItemDetails;
