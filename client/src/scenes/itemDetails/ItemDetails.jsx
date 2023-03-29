5088ea0fa067a340ba6abc89e042c1e3e5a296bd
Bryan Romero <bromero52@gmail.com>
2023-03-29 11:57:32 -0400

    Item Details page

--- client/src/scenes/itemDetails/ItemDetails.jsx
+++ client/src/scenes/itemDetails/ItemDetails.jsx
@@ -10,26 +10,37 @@ import { useNavigate, useParams } from "react-router-dom";
 import { useLocation } from "react-router-dom";
 
 const ItemDetails = () => {
+  // Gets the item id that is passed to the route
+  // E.g host/api/items/{itemId}
   let { itemId } = useParams();
 
-  const getItem = async () => {
-    const response = await fetch(`http://localhost:1337/api/items/${itemId}`, {
-      method: "GET",
-    });
+  const [item, setItem] = useState([]);
 
+  const getItem = async () => {
+    const response = await fetch(
+      `http://localhost:1337/api/items/${itemId}?populate=image`,
+      {
+        method: "GET",
+      }
+    );
     const jsonData = await response.json();
+    setItem(jsonData.data);
   };
 
   useEffect(() => {
     getItem();
-  }, []);
+  }, [itemId]);
 
   return (
     <Box width="80%" margin="80px auto">
       <Box display="flex" flexWrap="wrap" columnGap="40px">
         <Box backgroundColor="red" width="50"></Box>
 
-        <Box backgroundColor="red"></Box>
+        <Box backgroundColor="red">
+          <img
+            src={`http://localhost:1337${item?.attributes?.image?.data?.attributes?.url}`}
+          />
+        </Box>
       </Box>
     </Box>
   );
