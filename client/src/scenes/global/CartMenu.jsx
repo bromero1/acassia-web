df6aaab90ca3de7306c45c7adf731fb3e28b300b
Bryan Romero <bromero52@gmail.com>
2023-04-17 16:20:16 -0400

    prod api endpoint

--- client/src/scenes/global/CartMenu.jsx
+++ client/src/scenes/global/CartMenu.jsx
@@ -76,7 +76,7 @@ const CartMenu = () => {
                       alt={item?.name}
                       width="123px"
                       height="164px"
-                      src={`http://localhost:1337${item?.attributes?.image?.data?.attributes?.formats?.medium?.url}`}
+                      src={`https://luxuriant-rad-duck.strapiapp.com${item?.attributes?.image?.data?.attributes?.formats?.medium?.url}`}
                     />
                   </Box>
                   <Box flex="1 1 60%">
