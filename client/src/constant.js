a65457cc58da645f84122218ceaca4a779c30ef6
Bryan Romero <bromero52@gmail.com>
2023-04-21 13:36:37 -0400

    no pt

--- client/src/constant.js
+++ client/src/constant.js
@@ -1,5 +1,11 @@
 export const AVATAR_API = "https://ui-avatars.com/api";
-export const API = "https://moldy-tough-waste.strapiapp.com/api";
-export const HOST = "https://moldy-tough-waste.strapiapp.com"
 export const AUTH_TOKEN = "authToken";
 export const BEARER = "Bearer";
+
+// Cloud Endpoints
+export const API = "https://moldy-tough-waste.strapiapp.com/api";
+export const HOST = "https://moldy-tough-waste.strapiapp.com";
+
+// Local Endpoints for Dev
+// export const API = "http://localhost:1337/api";
+// export const HOST = "http://localhost:1337"
