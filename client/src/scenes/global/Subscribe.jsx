1a35ef98d34a4f87e458b3fa200d6041a53d8419
Bryan Romero <bromero52@gmail.com>
2023-04-27 00:20:10 -0400

    subscriber email list

--- client/src/scenes/global/Subscribe.jsx
+++ client/src/scenes/global/Subscribe.jsx
@@ -1,13 +1,33 @@
-import React from "react";
+import { useState } from "react";
 import { Box, Typography, TextField, Button } from "@mui/material";
 import { shades } from "../../theme";
 
 const Subscribe = () => {
+  const [email, setEmail] = useState("");
+  const [error, setError] = useState();
+
+  const emailChange = (event) => {
+    setEmail(event.target.value);
+  };
+
+  const subscribeToEmail = async () => {
+
+    try {
+      const response = await fetch("http://localhost:1337/api/subscribers", {
+        method: "PUT",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({data: {"email": email}}),
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <Box
       mt="0"
       width="100%"
-      backgroundColor={shades.secondary[200]}
+      backgroundColor={shades.secondary[300]}
       padding="80px auto"
     >
       <Box
@@ -15,14 +35,35 @@ const Subscribe = () => {
         flexDirection="column"
         //width="80%"
         paddingTop="30px"
-        paddingBottom                                                                                         ="30px"
+        paddingBottom="30px"
         alignItems="center"
       >
-        <Typography variant="h5" color={shades.primary[500]} mb="5px">
+        <Typography
+          variant="h5"
+          color={shades.primary[500]}
+          mb="15px"
+          letterSpacing="1.4px"
+        >
           NEWSLETTER SIGN-UP
         </Typography>
-        <TextField id="filled-basic" label="Email" variant="filled"  />
-        <Button variant="contained"  mt="50px" onClick={() => {}}>
+
+        <Box mb="10px">
+          <TextField
+            id="filled-basic"
+            label="Email"
+            variant="filled"
+            fullWidth
+            onChange={emailChange}
+          />
+        </Box>
+
+        <Button
+          variant="contained"
+          mt={12}
+          onClick={() => {
+            subscribeToEmail();
+          }}
+        >
           Submit
         </Button>
       </Box>
